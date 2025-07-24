ef80b53490c31fe8019d54969b9ad8bac6a9d099
Yashvardhan Singh <chanchalyashsingh2005@gmail.com>
2025-07-24 22:26:58 +0530

    Create util.py

---
+++ util.py
@@ -0,0 +1,14 @@
+import os
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+
+def derive_key(password: str, salt: bytes) -> bytes:
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    return kdf.derive(password.encode())
